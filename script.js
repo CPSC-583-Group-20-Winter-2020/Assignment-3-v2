f4fc8049348741f330dcc5dbdcc143ff383d36e2
nielsontrung <nielson.trung@ucalgary.ca>
2020-03-20 15:43:32 -0600

    adding stylesheet.css

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function mercator(x, y) {
-    return [x, Math.log(Math.tan(Math.PI / 4 + y / 2))];
-}
+import { select, json, geoPath, geoNaturalEarth1 } from 'd3';
+import { feature } from 'topojson';
+
+const svg = select('svg');
+
+const projection = geoNaturalEarth1();
+const pathGenerator = geoPath().projection(projection);
+
+svg.append('path')
+    .attr('class', 'sphere')
+    .attr('d', pathGenerator({type: 'Sphere'}));
+
+json('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
+    .then(data => {
+        const countries = feature(data, data.objects.countries);
+        svg.selectAll('path').data(countries.features)
+            .enter().append('path')
+            .attr('class', 'country')
+            .attr('d', pathGenerator);
+    });
